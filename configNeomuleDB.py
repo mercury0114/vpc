9532eb4ea433c4d02489924ddaad54e2e8aec46b
mercury0114 <mlatinis@gmail.com>
2019-05-17 12:53:05 +0300

    Adding config file

---
+++ configNeomuleDB.py
@@ -0,0 +1,21 @@
+gridtool_CONFIG = {
+'host': '192.168.192.69', 
+'database': 'wsi_grid_tool_devel', #table - locality_tiles
+'user': 'augren',
+'password': 'aure0870',
+}
+
+halosas_CONFIG = {
+'host': '192.168.192.69', 
+'database': 'halo_sas', #table - slide_ids_new
+'user': 'augren',
+'password': 'aure0870',
+}
+
+halo_CONFIG = {
+'host': '192.168.192.69', 
+'database': 'halo', #table - slide_ids_new
+'user': 'augren',
+'password': 'aure0870',
+}
+
