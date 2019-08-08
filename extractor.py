d781ead3896bc45df4a540087e97ce348c522daf
Marius Latinis <mariusl@neurotechnologija.local>
2019-08-08 13:59:36 +0300

    Added skeleton labelling method.

--- extractor.py
+++ extractor.py
@@ -15,24 +15,18 @@ def removeSmallCollagenBlops(oldFile, newFile):
         print(newFile + " already exists")
         return
     
-    success = False
-    try:
-        oldCollagen = memmap(oldFile, dtype='uint8')
-        newCollagen = memmap(newFile, shape = oldCollagen.shape, dtype='uint8')
-        for x in range(0, oldCollagen.shape[0] - (s-1), s/2):
-            for y in range(0, oldCollagen.shape[1] - (s-1), s/2):
-                patch = numpy.array(oldCollagen[x:x+s, y:y+s])
-                current = numpy.array(newCollagen[x:x+s, y:y+s])
-                newCollagen[x:x+s, y:y+s] = current | removeBlops(patch)
-        del oldCollagen
-        del newCollagen
-        success = True
-    finally:
-        if (not success):
-            os.remove(newFile)
+    oldCollagen = memmap(oldFile, dtype='uint8')
+    newCollagen = memmap(newFile, shape = oldCollagen.shape, dtype='uint8')
+    for x in range(0, oldCollagen.shape[0] - (s-1), s/2):
+        for y in range(0, oldCollagen.shape[1] - (s-1), s/2):
+            patch = numpy.array(oldCollagen[x:x+s, y:y+s])
+            current = numpy.array(newCollagen[x:x+s, y:y+s])
+            newCollagen[x:x+s, y:y+s] = current | removeBlops(patch)
+    del oldCollagen
+    del newCollagen
 
 def removeBlops(patch):
-    labelled, num = label(patch)
+    labelled, num = label(patch, structure = [[1,1,1],[1,1,1],[1,1,1]])
     counts = [0] * (num + 1)
     for x in range(patch.shape[0]):
         for y in range(patch.shape[1]):
@@ -49,22 +43,16 @@ def computeRawCollagenMask(slide, rawCollagenFile, model):
         print(rawCollagenFile + " already exists.")
         return
 
-    success = False
-    try:
-        rawCollagen = memmap(rawCollagenFile, shape=slide.dimensions, dtype='uint8')
-        for x in range(0, slide.dimensions[0] - (s-1), s/2):
-            for y in range(0, slide.dimensions[1] - (s-1), s/2):
-                patch = slide.read_region(location = (x, y), size = (s, s), level = 0)
-                patch = numpy.array(patch)[:,:,:3]
-                current = numpy.array(rawCollagen[x:x+s, y:y+s])
-                # openslide uses (width, height) dimensions, tifffile.memmap (height, width).
-                # Thus, I am transposing the extracted collagen.
-                rawCollagen[x:x+s, y:y+s] = current | numpy.transpose(extractCollagen(patch, model))
-        del rawCollagen
-        success = True
-    finally:
-        if (not success):
-            os.remove(rawCollagenFile)
+    rawCollagen = memmap(rawCollagenFile, shape=slide.dimensions, dtype='uint8')
+    for x in range(0, slide.dimensions[0] - (s-1), s/2):
+        for y in range(0, slide.dimensions[1] - (s-1), s/2):
+            patch = slide.read_region(location = (x, y), size = (s, s), level = 0)
+            patch = numpy.array(patch)[:,:,:3]
+            current = numpy.array(rawCollagen[x:x+s, y:y+s])
+            # openslide uses (width, height) dimensions, tifffile.memmap (height, width).
+            # Thus, I am transposing the extracted collagen.
+            rawCollagen[x:x+s, y:y+s] = current | numpy.transpose(extractCollagen(patch, model))
+    del rawCollagen
 
 def extractCollagen(patch, model):
     normalised = patch.astype(numpy.float) / 255
@@ -80,72 +68,14 @@ def fillHoles(oldCollagenFile, newCollagenFile):
     if (os.path.isfile(newCollagenFile)):
         print(newCollagenFile + " already exists.")
         return
-    success = False
-    try:
-        oldCollagen = memmap(oldCollagenFile, dtype='uint8')
-        newCollagen = memmap(newCollagenFile, shape=oldCollagen.shape, dtype='uint8')
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
-        newCollagen[:,:] = cv2.morphologyEx(oldCollagen, cv2.MORPH_CLOSE, kernel)
-        newCollagen[newCollagen == 1] = 255
-        del oldCollagen
-        del newCollagen
-        success = True
-    finally:
-        if (not success):
-            os.remove(newCollagenFile)
-
-def computeSkeleton(collagenFile, skeletonFile):
-    if (os.path.isfile(skeletonFile)): 
-        print(skeletonFile + " already exists.")
-        return
-    success = False
-    try:
-        collagen = memmap(collagenFile, dtype='uint8')
-        skeleton = memmap(skeletonFile, dtype='uint8', shape=collagen.shape)
-        skeleton[:,:] = collagen
-        del collagen
-        skeleton[skeleton == 255] = 1
-        skeleton[:,:] = skeletonize(skeleton)
-        skeleton[skeleton == 1] = 255
-        del skeleton
-        success = True
-    finally:
-        if (not success):
-            os.remove(skeletonFile)
-
-
-def partitionSkeleton(skeletonFile, partitionFile):
-    if (os.path.isfile(partitionFile)): 
-        print(partitionFile + " already exists.")
-        return
-    success = False
-    try:
-        skeleton = memmap(skeletonFile)
-        partition = memmap(partitionFile, shape=skeleton.shape, dtype='uint8')
-        partition[:,:] = skeleton
-        del skeleton
-        pixels = numpy.nonzero(partition)
-        for turn in [1,2,3]:
-            for pixel in range(len(pixels[0])):
-                n = countNeighbours(pixels[0][pixel], pixels[1][pixel], partition)
-                if (n > 2 or n == 0):
-                    partition[pixels[0][pixel], pixels[1][pixel]] = 0
-        del partition
-        success = True
-    finally:
-        if not success:
-            os.remove(partitionFile)
-
-def countNeighbours(x, y, image):
-    count = 0
-    for x0 in [-1,0,1]:
-        for y0 in [-1,0,1]:
-            if (x + x0 >= 0 and x + x0 < image.shape[0] and y + y0 >= 0 and y + y0 < image.shape[1]):
-                if (x0 != 0 or y0 != 0):
-                    if (image[x+x0, y+y0]):
-                        count += 1
-    return count;
-        
+    
+    oldCollagen = memmap(oldCollagenFile, dtype='uint8')
+    newCollagen = memmap(newCollagenFile, shape=oldCollagen.shape, dtype='uint8')
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+    newCollagen[:,:] = cv2.morphologyEx(oldCollagen, cv2.MORPH_CLOSE, kernel)
+    newCollagen[newCollagen == 1] = 255
+    del oldCollagen
+    del newCollagen
     
 def collagenAreaRatio(collagen):
     count = 0
--- skeletonizer.py
+++ skeletonizer.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import itertools
-import numpy as np
+import numpy
 import scipy
 import cv2
 
@@ -8,27 +8,92 @@ from skimage import io
 from skimage.morphology import skeletonize
 from math import sqrt
 from PIL import Image, ImageDraw
+from tifffile import memmap
+import os
 
-def countPixels(skeleton, i, j):
-	count = 0
-	for x, y in itertools.product((-1, 0, 1), (-1, 0, 1)):
-		if (i + x >= 0 and i + x < skeleton.shape[0] and j + y >= 0 and j + y < skeleton.shape[1]):
-			count += skeleton[i + x, j + y]
-	return count
+def computeSkeleton(collagenFile, skeletonFile):
+    if (os.path.isfile(skeletonFile)): 
+        print(skeletonFile + " already exists.")
+        return
+    
+    collagen = memmap(collagenFile, dtype='uint8')
+    skeleton = memmap(skeletonFile, dtype='uint8', shape=collagen.shape)
+    skeleton[:,:] = collagen
+    del collagen
+    skeleton[skeleton == 255] = 1
+    skeleton[:,:] = skeletonize(skeleton)
+    skeleton[skeleton == 1] = 255
+    del skeleton
+
+def partitionSkeleton(skeletonFile, partitionFile):
+    if (os.path.isfile(partitionFile)): 
+        print(partitionFile + " already exists.")
+        return
+
+    skeleton = memmap(skeletonFile)
+    partition = memmap(partitionFile, shape=skeleton.shape, dtype='uint8')
+    partition[:,:] = skeleton
+    del skeleton
+    pixels = numpy.nonzero(partition)
+    for turn in [1,2,3]:
+        for pixel in range(len(pixels[0])):
+            n = countNeighbours(partition, pixels[0][pixel], pixels[1][pixel])
+            if (n > 2):
+                partition[pixels[0][pixel], pixels[1][pixel]] = 0
+    del partition
+
+def countNeighbours(image, x, y):
+    count = 0
+    for x0, y0 in itertools.product((-1, 0, 1), (-1, 0, 1)):
+        if (x + x0 >= 0 and x + x0 < image.shape[0] and y + y0 >= 0 and y + y0 < image.shape[1]):
+            if (x0 != 0 or y0 != 0):
+                if (image[x+x0, y+y0]):
+                    count += 1
+    return count;
+
+def labelSkeletonParts(partitionFile, labelledFile):
+    if (os.path.isfile(labelledFile)): 
+        print(labelledFile + " already exists.")
+        return
+    
+    partition = memmap(partitionFile)
+    labelled = memmap(labelledFile, shape = partition.shape, dtype='uint32')
+    pixels = numpy.nonzero(partition)
+    newLabel = 1
+    for p in range(len(pixels[0])):
+        if (labelled[pixels[0][p], pixels[1][p]] == 0):
+            stack = [(pixels[0][p],pixels[1][p])]
+            while(len(stack) > 0):
+                x,y = stack.pop()
+                labelled[x,y] = newLabel
+                neighbours = findNeighbours(partition, x, y)
+                for n in neighbours:
+                    if (labelled[n[0], n[1]] == 0):
+                        stack.append(n)
+            newLabel += 1
+    del partition
+    del labelled
 
 def junction(skeleton, i, j):
-	return countPixels(skeleton, i, j) > 3
+	return countNeighbours(skeleton, i, j) > 2
 
 def corner(skeleton, i, j):
-	return countPixels(skeleton, i, j) == 2
+	return countNeighbours(skeleton, i, j) == 1
+
+def findNeighbours(skeleton, x, y):
+    neighbours = []
+    for x0, y0 in itertools.product((-1, 0, 1), (-1, 0, 1)):
+        if (x + x0 >= 0 and x + x0 < skeleton.shape[0] and
+            y + y0 >= 0 and y + y0 < skeleton.shape[1] and skeleton[x + x0, y + y0]):
+            if (x != 0 or y != 0):
+                neighbours.append((x + x0, y + y0))
+    return neighbours
 
 def findNeighbour(skeleton, i, j):
-    for x, y in itertools.product((-1, 0, 1), (-1, 0, 1)):
-        if (i + x >= 0 and i + x < skeleton.shape[0] and
-            j + y >= 0 and j + y < skeleton.shape[1] and skeleton[i + x, j + y]):
-            if (x != 0 or y != 0):
-                return (i + x, j + y)
-    return False
+    neighbours = findNeighbours(skeleton, i, j)
+    if (len(neighbours) == 0):
+        return False
+    return neighbours[0]
 
 def countAndRemovePixels(skeleton, pixel):
 	count = 0
@@ -61,7 +126,7 @@ def triangle(skeleton, i, j):
 	return False
 
 def averageEdgeLength(s):
-	skeleton = np.copy(s)
+	skeleton = numpy.copy(s)
 	# Splitting skeleton into edges
 	for i,j in itertools.product(range(skeleton.shape[0]), range(skeleton.shape[1])):
 		if skeleton[i, j] and triangle(skeleton, i, j):
@@ -84,7 +149,7 @@ def averageEdgeLength(s):
 
 def getCurvature(s, pixel):
 	length = 0
-	first = np.copy(pixel)
+	first = numpy.copy(pixel)
 	n = findNeighbour(s, pixel[0], pixel[1])
 	while(n):
 		s[pixel[0], pixel[1]] = False
@@ -97,7 +162,7 @@ def getCurvature(s, pixel):
 	return sqrt((first[0] - pixel[0])**2 + (first[1] - pixel[1])**2) / length
 
 def averageCurvature(s):
-	skeleton = np.copy(s)
+	skeleton = numpy.copy(s)
 	# Splitting skeleton into edges
 	for i,j in itertools.product(range(skeleton.shape[0]), range(skeleton.shape[1])):
 		if skeleton[i, j] and triangle(skeleton, i, j):
@@ -119,17 +184,17 @@ def averageCurvature(s):
 def shortestDistance(i, j, collagen):
 	bottom = (max(0, i - 50), max(0, j - 50))
 	top = (min(i + 50, collagen.shape[0]), min(j + 50, collagen.shape[1]))
-	area = np.array(collagen[bottom[0] : top[0], bottom[1] : top[1]])
-	areaCopy = np.copy(area)
+	area = numpy.array(collagen[bottom[0] : top[0], bottom[1] : top[1]])
+	areaCopy = numpy.copy(area)
 	areaCopy[area == 0] = 1
 	areaCopy[area != 0] = 0
-	pixels = np.stack(np.nonzero(areaCopy), axis = -1)
+	pixels = numpy.stack(np.nonzero(areaCopy), axis = -1)
 	# pixels = cv2.findNonZero(areaCopy)
 	if (len(pixels) == 0):
 		return 60
 	centerX = area.shape[0] / 2
 	centerY = area.shape[1] / 2
-	distances = np.sqrt((pixels[:,0] - centerX)**2 + (pixels[:,1] - centerY)**2)
+	distances = numpy.sqrt((pixels[:,0] - centerX)**2 + (pixels[:,1] - centerY)**2)
 	return min(distances)
 
 def averageWidth(skeleton, collagen):
@@ -144,15 +209,15 @@ def averageWidth(skeleton, collagen):
 	return width / count
 
 def drawhex(w=260,fill=1,inv=0):
-    h = int(np.sqrt(3)*.5*260 / 2) * 2
+    h = int(numpy.sqrt(3)*.5*260 / 2) * 2
     polygon = [(0,h/2),(w/4,0),(3*w/4,0),(w,h/2),(3*w/4,h),(w/4,h)]
     img_ = Image.new('L', (w, h), 0)
     ImageDraw.Draw(img_).polygon(polygon, outline=1, fill=fill)
-    img = np.array(img_)
+    img = numpy.array(img_)
     if inv != 0:
         img = ImageOps.invert(img_)
-        img = np.array(img) / 255.
-    return(img.astype(np.uint8))
+        img = numpy.array(img) / 255.
+    return(img.astype(numpy.uint8))
 emptyHex = drawhex()
 
 def depth(collagen, centerX, centerY):
