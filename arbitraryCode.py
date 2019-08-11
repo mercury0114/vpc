93551377005a2882936de6f21c240ba124625181
Marius <m.latinis@gmail.com>
2019-08-11 19:38:40 +0300

    Newest code.

--- arbitraryCode.py
+++ arbitraryCode.py
@@ -28,13 +28,3 @@ print("Labelled into " + str(numberOfParts) + " parts.")
 print("Splitting collagen into fibers")
 fibersFile = outdir + "fibers.tiff"
 makeFibersFromSkeleton(collagenHolesFilledFile, labelledFile, fibersFile)
-
-
-from tifffile import memmap
-from fiber import extractOneFiber
-from fiber import length
-from fiber import width
-from PIL import Image
-p = memmap("./../data/masks/smaller/pvz.tiff")
-fiber = extractOneFiber(p, 572, 350)
-Image.fromarray(fiber).save("./../data/masks/smaller/temp.png")
