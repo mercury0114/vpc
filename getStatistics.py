a996045f3624a42d89f5066348ab0b217d4f86b4
mercury0114 <marius@DMSTI.GPU>
2019-08-27 11:47:15 +0300

    Adding statistics aggregation.

--- getStatistics.py
+++ getStatistics.py
@@ -3,6 +3,7 @@ import openslide
 import os
 import numpy
 import mahotas
+import pandas
 from numpy import memmap
 from scipy.misc import imsave
 from skeletonizer import *
@@ -31,8 +32,10 @@ def drawhex():
 emptyHex = drawhex()
 
 statisticsHeader ="hexX,hexY,class,distance,length,width,areaRatio,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13\n"
+aggregatedHeader ="class,distance,length,width,areaRatio,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13\n"
 
-def flushStatisticsToFile(collagen, center, f):
+
+def getStatistics(collagen, center):
     statistics = center[:]
     statistics.append(length(collagen))
     statistics.append(width(collagen))
@@ -40,9 +43,34 @@ def flushStatisticsToFile(collagen, center, f):
     copy = numpy.copy(collagen)
     copy[copy > 0] = 1
     statistics.extend([e for e in mahotas.features.haralick(copy).mean(0)])
+    return statistics
+
+def flushToFile(statistics, f):
     f.write(','.join(str(e) for e in statistics))
     f.write('\n')
     f.flush()
+
+def aggregateStatistics(fullFile, aggregatedFile):
+    data = pandas.read_csv(fullFile)
+    counts = {}
+    sums = {}
+    for index, row in data.iterrows():
+        key = int(row['class']), int(row['distance'])
+        values = numpy.array(row[4:].values.tolist())
+        if key not in counts:
+            counts[key] = 1
+            sums[key] = values
+        else:
+            counts[key] += 1
+            sums[key] += values
+
+    aggregated = open(aggregatedFile, "w")
+    aggregated.write(aggregatedHeader)
+    for key in counts:
+        sums[key] /= counts[key]
+        statistics = list(key) + list(sums[key])
+        flushToFile(statistics, aggregated)
+    aggregated.close()
 
 grids = ['8286']
 
@@ -52,14 +80,16 @@ for gID in grids:
     fibers = memmap("./../data/masks/" + str(gID) + "/fibers.tiff")
     grid = getGrid(gID)
     directory = "./../data/statistics/" + str(gID) + "/"
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
+    if (os.path.isdir(directory)):
+        continue
+    os.mkdir(directory)
 
     hexFile = open(directory + "hex.txt", "w")
     fiberFile = open(directory + "fiber.txt", "w")
     hexFile.write(statisticsHeader)
     fiberFile.write(statisticsHeader)
     
+    computedFiberStatistics = {}
     for center in grid:
         c = [int(e) for e in center]
         if (c[2] != 3 and c[2] != 5 and 
@@ -69,19 +99,25 @@ for gID in grids:
             patch = numpy.array(patch)
             hexagon = emptyHex & patch
             if (hexagon.max() > 0):
-                flushStatisticsToFile(hexagon, c, hexFile)
+                hexStatistics = getStatistics(hexagon, c)
+                flushToFile(hexStatistics, hexFile)
 
                 processedFiber = {}
                 nonzero = hexagon.nonzero()
                 for n in range(len(nonzero[0])):
                     fiberId = hexagon[nonzero[0][n], nonzero[1][n]]
                     if fiberId not in processedFiber:
-                        fiber = extractOneFiber(fibers, c[0] - h//2 + nonzero[0][n], c[1] - w//2 + nonzero[1][n])
-                        flushStatisticsToFile(fiber, c, fiberFile)
+                        if fiberId not in computedFiberStatistics:
+                            fiber = extractOneFiber(fibers, c[0] - h//2 + nonzero[0][n], c[1] - w//2 + nonzero[1][n])
+                            computedFiberStatistics[fiberId] = getStatistics(fiber, c)
+                        flushToFile(computedFiberStatistics[fiberId], fiberFile)
                         processedFiber[fiberId] = True
     
     hexFile.close()
     fiberFile.close()
+
+    aggregateStatistics(directory + "hex.txt", directory + "aggregated_hex.txt")
+    aggregateStatistics(directory + "fiber.txt", directory + "aggregated_fiber.txt")
     print('DONE with ', gID)
 
 print(time.time() - start)
