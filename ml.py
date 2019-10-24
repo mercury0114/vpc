5af91094602c25fdb1be7e9cd43a9bd54a97b6e4
mercury0114 <marius@DMSTI.GPU>
2019-10-24 12:38:46 +0300

    Committing ml file.

--- ml.py
+++ ml.py
@@ -3,10 +3,20 @@ import csv
 f = open("./../data/statistics/merged_fiber.txt", "r")
 next(f)
 data = [[float(x) for x in line.split(",")] for line in f]
-
 dictionary = {}
 for row in data:
-    if (row[1] == 0):
-        dictionary[row[0]] = row[2:]
+    if (int(row[1]) == 0):
+        dictionary[int(row[0])] = row[2:]
 
-print(len(dictionary))
+f = open("./../data/survival.txt")
+survival = [line.split(",") for line in f]
+output = {}
+for entry in survival:
+    output[int(entry[0])] = int(entry[2])
+
+X = []
+y = []
+for key in output.keys():
+    X.append(dictionary[key])
+    y.append(output[key])
+
